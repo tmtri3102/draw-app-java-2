66f038e41187f0bc190d3307dda541a56fff9108
tomeytran <tmtri310251@gmail.com>
2024-10-10 15:30:09 +0700

    draw shapes

--- src/DrawApp.java
+++ src/DrawApp.java
@@ -32,8 +32,41 @@ public class DrawApp {
                 break;
             case 3:
                 for (int i = 0; i < 5; i++) {
-                    for(int j = 0; j< 5 - i; j++) {
-                        System.out.print("* ");
+                    for(int j = 0; j < 5 - i; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println(" ");
+                }
+                break;
+            case 5:
+                for (int i = 0; i < 5; i++) {
+                    for(int j = 0; j< i; j++) {
+                        System.out.print(" ");
+                    }
+//                    System.out.println(" ");
+                    for(int j = 0; j< 5-i; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println(" ");
+                }
+                break;
+            case 6:
+//                for (int i = 0; i < 5; i++) {
+//                    for(int j = 5; j > 0; j--) {
+//                        System.out.print(" ");
+//                    }
+//                    for (int j = i+1; j > 0; j--) {
+//                        System.out.print("*");
+//
+//                    }
+//                    System.out.println(" ");
+//                }
+                for (int i = 0; i < 5; i++) {
+                    for(int j = 0; j <= 5 - i; j++) {
+                        System.out.print(" ");
+                    }
+                    for(int j = 0; j <= i; j++) {
+                        System.out.print("*");
                     }
                     System.out.println(" ");
                 }
